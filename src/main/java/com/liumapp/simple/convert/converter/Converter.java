c1e46c272858b5737dab021180e29b27422c9cb6
liumapp <liumapp.com@gmail.com>
2019-04-12 16:33:57 +0800

    updaate converter class

--- src/main/java/com/liumapp/simple/convert/converter/Converter.java
+++ src/main/java/com/liumapp/simple/convert/converter/Converter.java
@@ -1,5 +1,8 @@
 package com.liumapp.simple.convert.converter;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * file Converter.java
  * author liumapp
@@ -10,6 +13,15 @@ package com.liumapp.simple.convert.converter;
  */
 public interface Converter {
 
+    /**
+     * 根据文档的地址进行转换
+     * @param sourcePath 要转换的文档地址
+     * @param destPath 转换后的文档地址
+     */
+    public void convertByFilePath (String sourcePath, String destPath);
 
+    public OutputStream convertByStream (InputStream inputStream);
+
+    public String convertByBase64 (String sourceBase64);
 
 }
